df4f2a2881546e47c901a1350be2e52533b1ccf2
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-26 18:19:26 +0100

    Comment out old code

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// Variables for calculations
-let num1 = 5;
-let operator = "+";
-let num2 = 3;
+// // Variables for calculations
+// let num1 = 5;
+// let operator = "+";
+// let num2 = 3;
 
-function addInput(a, b) {
-  return a + b;
-}
+// function addInput(a, b) {
+//   return a + b;
+// }
 
-function subtractInput(a, b) {
-  return a - b;
-}
+// function subtractInput(a, b) {
+//   return a - b;
+// }
 
-function multiplyInput(a, b) {
-  return a * b;
-}
+// function multiplyInput(a, b) {
+//   return a * b;
+// }
 
-function divideInput(a, b) {
-  if (b === 0) {
-    return "Error";
-  } else {
-    return a / b;
-  }
-}
+// function divideInput(a, b) {
+//   if (b === 0) {
+//     return "Error";
+//   } else {
+//     return a / b;
+//   }
+// }
 
-function operate(operator, a, b) {
-  if (operator === "+") {
-    return addInput(a, b);
-  } else if (operator === "-") {
-    return subtractInput(a, b);
-  } else if (operator === "*") {
-    return multiplyInput(a, b);
-  } else if (operator === "/") {
-    return divideInput(a, b);
-  } else {
-    return "Error";
-  }
-}
+// function operate(operator, a, b) {
+//   if (operator === "+") {
+//     return addInput(a, b);
+//   } else if (operator === "-") {
+//     return subtractInput(a, b);
+//   } else if (operator === "*") {
+//     return multiplyInput(a, b);
+//   } else if (operator === "/") {
+//     return divideInput(a, b);
+//   } else {
+//     return "Error";
+//   }
+// }
 
 console.log(operate(operator, num1, num2));
 

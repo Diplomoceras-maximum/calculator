3fdcd3fa9c7c12c5a46240b7aa9827549987f8aa
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-26 17:41:41 +0100

    Add code to function that take 2 numbers are return the value

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ let num1 = "";
 let operator = "";
 let num2 = "";
 
-function addInput() {}
+function addInput(a, b) {
+  return a + b;
+}
 
-function subtractInput() {}
+function subtractInput(a, b) {
+  return a - b;
+}
 
-function multiplyInput() {}
+function multiplyInput(a, b) {
+  return a * b;
+}
 
-function divideInput() {}
+function divideInput(a, b) {
+  if (b === 0) {
+    return "Error";
+  } else {
+    return a / b;
+  }
+}
